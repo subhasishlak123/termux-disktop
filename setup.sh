88dcfea0fdb804239f663d5b941d416bcfa599f1
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2023-01-26 13:11:41 +0530

    1st

---
+++ setup.sh
@@ -0,0 +1,9 @@
+cd ~/termux-disktop/
+mkdir .config
+mv i3 .config
+mv i3status
+mkdir .fonts
+mv Sauce Code Pro Nerd Font Complete.ttf .fonts
+mv Victor Mono Regular Nerd Font Complete.ttf .fonts
+chmod +x .config/i3/config
+chmod +x .config/i3status/config
