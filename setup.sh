525fa51facfda55164b54c392430e4a2a17abd49
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2023-01-30 22:06:02 +0530

    2nd

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 echo "updating . . ."
-pkg update
+apk update
 echo "upgradeing . . ."
-pkg upgrade
-echo "installing x11-repo for x"
-pkg install x11-repo
+apk upgrade
 echo "installing pkges for desktop"
-pkg install qterminal i3 i3status dmenu feh git neovim tigervnc caja
+apk add qterminal i3 i3status dmenu feh git neovim tigervnc caja
 git clone https://github.com/subhasishlak123/termux-disktop
 echo "going to termux-disktop"
-cd ~/termux-disktop/
+
 echo "created  .config"
 mkdir .config
-echo "moving files form termux-desktop to .config"
-mv i3 .config
-mv i3status
-echo "created .fonts"
 mkdir .fonts
-mv Sauce Code Pro Nerd Font Complete.ttf .fonts
-mv Victor Mono Regular Nerd Font Complete.ttf .fonts
+echo "moving files form termux-desktop to .config"
+cd ~/termux-disktop/
+mv i3 ~/.config
+mv i3status ~/.config
+echo "created .fonts"
+
+mv Sauce Code Pro Nerd Font Complete.ttf ../fonts
+mv Victor Mono Regular Nerd Font Complete.ttf ../fonts
 chmod +x ~/.config/i3/config
 chmod +x ~/.config/i3status/config
 mv pic ..
